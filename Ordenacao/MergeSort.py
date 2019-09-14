7829eb18bc753f1dd099f4b2e2bb93eb28b233f6
DURAND <junior_durand@outlook.com>
2019-09-14 15:12:33 -0300

    .

--- Ordenacao/MergeSort.py
+++ Ordenacao/MergeSort.py
@@ -1,48 +1,12 @@
 class MergeSort(object):
 
-"""
-	def sort(self, col): 
-		if len(col) >1:
-			mid = len(col)//2 #Finding the mid of the colay 
-			colL = col[:mid] # Dividing the colay elements  
-			colR = col[mid:] # into 2 halves 
 
-			self.sort(colL) 
-			self.sort(colR) 
-
-			i = 0
-			j = 0
-			k = 0
-
-			while i < len(colL) and j < len(colR): 
-				if colL[i] < colR[j]: 
-					col[k] = colL[i] 
-					i+=1
-				else: 
-					col[k] = colR[j] 
-					j+=1
-				k+=1
-
-			# Checking if any element was left 
-			while i < len(colL): 
-				col[k] = colL[i] 
-				i+=1
-				k+=1
-
-			while j < len(colR): 
-				col[k] = colR[j] 
-				j+=1
-				k+=1
-
-
-
-"""
 
 	def sort(self,  col, start = 0, end = None): 
 		if len(col) >1:
-			mid = len(col)//2 #Finding the mid of the colay 
-			colL = col[:mid] # Dividing the colay elements  
-			colR = col[mid:] # into 2 halves 
+			mid = len(col)//2 
+			colL = col[:mid]  
+			colR = col[mid:]
 
 			self.sort(colL) 
 			self.sort(colR) 
@@ -60,7 +24,6 @@ class MergeSort(object):
 					j+=1
 				k+=1
 
-			# Checking if any element was left 
 			while i < len(colL): 
 				col[k] = colL[i] 
 				i+=1
