932abb31038233c87bd47211a3f564c069ded558
Durand <junior_durand@outlook.com>
2019-12-08 14:10:55 -0300

    create a main for test.

---
+++ Graph adjacency list/main.py
@@ -0,0 +1,18 @@
+from graph import Graph
+
+myGraph = Graph(5, oriented = "oriented")
+#myGraph = Graph(5)
+
+myGraph.addEDGE(1,4)
+myGraph.addEDGE(1,3)
+myGraph.addEDGE(2,3)
+myGraph.addEDGE(2,4)
+myGraph.addEDGE(3,4)
+
+
+
+print(myGraph)
+
+myGraph.removeEDGE(3,4)
+
+print(myGraph)
