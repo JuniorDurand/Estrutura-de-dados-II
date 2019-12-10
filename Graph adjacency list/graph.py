7743e77829d63ac3382c4c7c3e46b936e50bf2de
Durand <Junior_durand@outlook.com>
2019-12-10 18:26:45 -0300

    fix Graph adjacency list.

--- Graph adjacency list/graph.py
+++ Graph adjacency list/graph.py
@@ -18,6 +18,17 @@ class Vertex(object):
 			self.listAdj.pop(index)
 			self.listWeight.pop(index)
 
+	def containsEDGE(self, edge):
+		if edge in self.listAdj:
+			return True
+		else:
+			return False
+
+	def updateEDGE(self, edge, weight=1):
+		index = self.listAdj.index(edge)
+		if not index is None:
+			self.listWeight[index] = weight
+
 	def __str__(self):
 		string = ""
 		string += "%d :\n" % (self.num)
@@ -39,18 +50,21 @@ class Graph(object):
 
 
 	def addEDGE(self, u, v, weight = None):
-		if self.oriented == "not_oriented":
-			if weight == None:
-				self.vertex[u].addEDGE(self.vertex[v])
-				self.vertex[v].addEDGE(self.vertex[u])
+		if not self.containsEDGE(u, v):
+			if self.oriented == "not_oriented":
+				if weight == None:
+					self.vertex[u].addEDGE(self.vertex[v])
+					self.vertex[v].addEDGE(self.vertex[u])
+				else:
+					self.vertex[u].addEDGE(self.vertex[v], weight = weight)
+					self.vertex[v].addEDGE(self.vertex[u], weight = weight)
 			else:
-				self.vertex[u].addEDGE(self.vertex[v], weight = weight)
-				self.vertex[v].addEDGE(self.vertex[u], weight = weight)
+				if weight == None:
+					self.vertex[u].addEDGE(self.vertex[v])
+				else:
+					self.vertex[u].addEDGE(self.vertex[v], weight = weight)
 		else:
-			if weight == None:
-				self.vertex[u].addEDGE(self.vertex[v])
-			else:
-				self.vertex[u].addEDGE(self.vertex[v], weight = weight)
+			self.updateEDGE(u, v, weight)
 
 	
 	def removeEDGE(self, u, v):
@@ -59,9 +73,21 @@ class Graph(object):
 			self.vertex[v].removeEDGE(self.vertex[u])
 		else:
 			self.vertex[u].removeEDGE(self.vertex[v])
+
+
+	def updateEDGE(self, u, v, weight = 1):
+		if self.containsEDGE(u,v):
+			if self.oriented == "not_oriented":
+				self.vertex[u].updateEDGE(self.vertex[v], weight)
+				self.vertex[v].updateEDGE(self.vertex[u], weight)
+			else:
+				self.vertex[u].updateEDGE(self.vertex[v], weight)
+
+
+
 	
 	def containsEDGE(self, u, v):
-		return True if self.vertex[u].listAdj.index(self.vertex[v]) != None else False
+		return True if self.vertex[u].containsEDGE(self.vertex[v]) == True else False
 
 
 	def __len__(self):
