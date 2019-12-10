5a99e4df97f736d6caabe35d29c52b1e1d0653ab
Durand <Junior_durand@outlook.com>
2019-12-10 19:06:34 -0300

    update main.

--- Graph adjacency list/main.py
+++ Graph adjacency list/main.py
@@ -1,18 +1,13 @@
 from graph import Graph
 
-myGraph = Graph(5, oriented = "oriented")
-#myGraph = Graph(5)
+#myGraph = Graph(5, oriented = "oriented")
+myGraph = Graph(5)
 
-myGraph.addEDGE(1,4)
-myGraph.addEDGE(1,3)
-myGraph.addEDGE(2,3)
+myGraph.addEDGE(1,2)
 myGraph.addEDGE(2,4)
-myGraph.addEDGE(3,4)
-
+myGraph.addEDGE(2,3)
 
 
 print(myGraph)
 
-myGraph.removeEDGE(3,4)
-
-print(myGraph)
+myGraph.BFS(u=1)
