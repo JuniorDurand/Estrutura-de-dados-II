7bdf96fdcbe5b2d0640dd3c0e41817eb582f5574
Durand <junior_durand@outlook.com>
2019-12-14 21:26:44 -0300

    update a main for test.

--- Graph adjacency list/main.py
+++ Graph adjacency list/main.py
@@ -3,14 +3,18 @@ from graph import Graph
 #myGraph = Graph(5, oriented = "oriented")
 myGraph = Graph(6)
 
-myGraph.addEDGE(1,2)
-myGraph.addEDGE(2,5)
-myGraph.addEDGE(5,3)
-myGraph.addEDGE(4,5)
-myGraph.addEDGE(1,5)
+myGraph.addEDGE(1,2,weight= 1)
+myGraph.addEDGE(2,5,weight= 2)
+myGraph.addEDGE(5,3,weight= 7)
+myGraph.addEDGE(4,3,weight= 2)
+myGraph.addEDGE(1,5,weight= 4)
 
 
 print(myGraph)
 
-myGraph.BFS(u=1)
-myGraph.BFS_orig_dest( 1, 4)
+
+myGraph.Prim(u=1)
+
+print(myGraph.MTS)
+myGraph.Kruskal()
+print(myGraph.MTS)
